fd806b054dfb4806dff0dd36ce61f8345481e652
DiemCode <luoibieng19@gmail.com>
2025-04-22 04:43:07 +0700

    add_tien_trinh_tuan_2

---
+++ Tuan2_View_And_Model/Phan_Mem_Quan_Ly_Cham_Diem_Hoi_Thi_Dieu_Lenh/Phan_Mem_Quan_Ly_Cham_Diem_Hoi_Thi_Dieu_Lenh/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Phan_Mem_Quan_Ly_Cham_Diem_Hoi_Thi_Dieu_Lenh
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Tuan2_View_And_Model/Phan_Mem_Quan_Ly_Cham_Diem_Hoi_Thi_Dieu_Lenh/Phan_Mem_Quan_Ly_Cham_Diem_Hoi_Thi_Dieu_Lenh/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Phan_Mem_Quan_Ly_Cham_Diem_Hoi_Thi_Dieu_Lenh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
